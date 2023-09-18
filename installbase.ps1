53253652b9fe484d13b77534f510c19774176b70
tommieklk <87391969+tommieklk@users.noreply.github.com>
2023-09-18 19:53:36 +0200

    Update installbase.ps1

--- installbase.ps1
+++ installbase.ps1
@@ -20,14 +20,14 @@ If($AppInstaller.Version -lt "1.20.2201.0") {
 
         Set-Location C:\ProgramData\EOO\WinGetPackages
 
-    $progressPreference = 'silentlyContinue'
-    Write-Information "Downloading WinGet and its dependencies..."
-    Invoke-WebRequest -Uri https://github.com/Easy-Office-Online/Winget/blob/main/Winget-install/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appx -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-    Invoke-WebRequest -Uri https://github.com/Easy-Office-Online/Winget/blob/main/Winget-install/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Invoke-WebRequest -Uri https://github.com/Easy-Office-Online/Winget/blob/main/Winget-install/Microsoft.UI.Xaml.2.8.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
-    Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-    Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading WinGet and its dependencies..."
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
         Write-Host "Starting sleep for Winget to initiate" -Foregroundcolor Yellow
         Start-Sleep 2
